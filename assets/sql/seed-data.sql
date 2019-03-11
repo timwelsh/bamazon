bdf626f3b2c0eb9736f3bc7d719baebcab99edb9
Tim Welsh <tim@timwelsh.com>
2019-03-10 22:54:43 -0400

    update sql

--- assets/sql/seed-data.sql
+++ assets/sql/seed-data.sql
@@ -3,12 +3,23 @@ use bamazonDB;
 insert into products (product_name, department_name, price, stock_quantity)
 values ("MacBook Pro", "Laptops", 1000.00, 12);
 insert into products (product_name, department_name, price, stock_quantity)
-values ("Samsung Galaxy S8", "Cell Phones", 800.00, 15);
+values ("Samsung Galaxy S8", "Cell Phones", 800.00, 150);
 insert into products (product_name, department_name, price, stock_quantity)
-values ("iPhone 8", "Cell Phones", 350.00, 18);
+values ("iPhone 8", "Cell Phones", 350.00, 180);
 insert into products (product_name, department_name, price, stock_quantity)
 values ("HP laptop", "Laptops", 500.00, 120);
 insert into products (product_name, department_name, price, stock_quantity)
-values ("Dell Laptop", "Laptops", 850.00, 52);
+values ("Dell Laptop", "Laptops", 850.00, 520);
+
+insert into products (product_name, department_name, price, stock_quantity)
+values ("Alienware", "Laptops", 1500.00, 120);
+insert into products (product_name, department_name, price, stock_quantity)
+values ("Google Phone", "Cell Phones", 900.00, 150);
+insert into products (product_name, department_name, price, stock_quantity)
+values ("iPhone X", "Cell Phones", 950.00, 380);
+insert into products (product_name, department_name, price, stock_quantity)
+values ("Lenovo", "Laptops", 800.00, 220);
+insert into products (product_name, department_name, price, stock_quantity)
+values ("ASUS", "Laptops", 850.00, 520);
 
 select * from products;
